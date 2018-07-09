46f3879159e187fac88ad9d42d5a920e2f7b8fe4
= <michaelhagarty@gmail.com>
2018-07-09 21:38:26 +1000

    Save account transactions.

--- account/repository.py
+++ account/repository.py
@@ -4,6 +4,7 @@ from typing import Optional
 from uuid import UUID
 
 from .account import Account, ExternalCounterparty, RegularAccount
+from .transaction import Transaction
 
 
 class AccountType(Enum):
@@ -40,6 +41,15 @@ class AccountRepository:
 
         self.database.insert(ACCOUNT_MODEL, account_record)
 
+        for t in account.transactions:
+            transaction_record = {'id': t.id,
+                                  'reference': t.reference,
+                                  'amount': t.amount,
+                                  'type': t.type,
+                                  'account': account.id}
+
+            self.database.insert(TRANSACTION_MODEL, transaction_record)
+
     def get(self, account_id: UUID) -> Optional[Account]:
         try:
             account_record = self.database.get(ACCOUNT_MODEL, account_id)
@@ -56,9 +66,19 @@ class AccountRepository:
             raise ValueError('Type {} does not match a valid account type.'
                              .format(account_record['type']))
 
+        transaction_records = self.database.find(
+            TRANSACTION_MODEL,
+            lambda t: t['account'] == account_id
+        )
+        transactions = []
+        for t in transaction_records:
+            t.pop('account', None)
+            transactions.append(Transaction(**t))
+
         return account_class(id=account_record['id'],
                              owner=account_record['owner'],
-                             balance=account_record['balance'])
+                             balance=account_record['balance'],
+                             transactions=transactions)
 
     def update(self, account: Account) -> None:
         account_record = {'id': account.id,
@@ -67,3 +87,13 @@ class AccountRepository:
                           'type': account_types[type(account)]}
 
         self.database.update(ACCOUNT_MODEL, account_record)
+
+        for t in account.transactions:
+            self.database.delete(TRANSACTION_MODEL, t.id)
+            transaction_record = {'id': t.id,
+                                  'reference': t.reference,
+                                  'amount': t.amount,
+                                  'type': t.type,
+                                  'account': account.id}
+
+            self.database.insert(TRANSACTION_MODEL, transaction_record)
