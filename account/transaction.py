6b8234ed3a55a4b3ceb5963db830561d8dc1d1d7
= <michaelhagarty@gmail.com>
2018-07-09 20:10:25 +1000

    Allow deep copy.

--- account/transaction.py
+++ account/transaction.py
@@ -1,4 +1,8 @@
 
+from __future__ import annotations
+
+from copy import deepcopy
+
 from enum import auto, Enum
 from uuid import UUID
 
@@ -21,3 +25,19 @@ class Transaction(Entity):
 
         self.amount = amount if amount is not None else AUD('0')
         self.type = type
+
+    def __copy__(self) -> Transaction:  # noqa
+        return type(self)(id=self.id, amount=self.amount, type=self.type)
+
+    def __deepcopy__(self, memo) -> Transaction: # noqa
+        self_id = id(self)
+        if self_id in memo:
+            return memo[self_id]
+
+        new_id = deepcopy(self.id)
+        new_amount = deepcopy(self.amount)
+        new_type = deepcopy(self.type)
+        new_transaction = type(self)(id=new_id, amount=new_amount,
+                                     type=new_type)
+
+        return new_transaction
