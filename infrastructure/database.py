ad80bf2c113960232a82a235319ffad3a1d6faaa
= <michaelhagarty@gmail.com>
2018-07-09 21:38:00 +1000

    Add Record type. Add find and delete methods to database.

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -2,10 +2,12 @@
 from __future__ import annotations
 
 from copy import deepcopy
-from typing import Any, Dict
+from typing import Any, Callable, Dict, List
 from uuid import UUID
 
-_database: Dict[str, Dict[UUID, dict]] = {}
+Record = Dict[str, Any]
+
+_database: Dict[str, Dict[UUID, Record]] = {}
 
 
 class AlreadyExists(Exception):
@@ -18,21 +20,21 @@ class NoMatchingRecord(Exception):
 
 class Database:
     @classmethod
-    def insert(cls, model_name: str, record: Dict[str, Any]) -> None:
+    def insert(cls, model_name: str, record: Record) -> None:
         if cls.exists(model_name, record['id']):
             raise AlreadyExists('Cannot insert over an existing ID.')
 
         cls.write_record(model_name, record)
 
     @classmethod
-    def update(cls, model_name: str, record: Dict[str, Any]) -> None:
+    def update(cls, model_name: str, record: Record) -> None:
         if not cls.exists(model_name, record['id']):
             raise NoMatchingRecord('Cannot update non-existent record.')
 
         cls.write_record(model_name, record)
 
     @classmethod
-    def get(cls, model_name: str, model_id: UUID) -> Dict[str, Any]:
+    def get(cls, model_name: str, model_id: UUID) -> Record:
         if not cls.exists(model_name, model_id):
             raise NoMatchingRecord('No record found.')
 
@@ -43,7 +45,18 @@ class Database:
         return model_id in _database.get(model_name, {})
 
     @classmethod
-    def write_record(cls, model_name: str, record: Dict[str, Any]) -> None:
+    def find(cls, model_name: str,
+             predicate: Callable[[Record], bool] = lambda m: True) -> List[Record]:  # noqa
+        return [deepcopy(r)
+                for r in _database.get(model_name, {}).values()
+                if predicate(r)]
+
+    @classmethod
+    def delete(cls, model_name: str, model_id: UUID) -> None:
+        _database.get(model_name, {}).pop(model_id, None)
+
+    @classmethod
+    def write_record(cls, model_name: str, record: Record) -> None:
         records = _database.setdefault(model_name, {})
         record = deepcopy(record)
         records[record['id']] = record
