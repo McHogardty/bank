02f54cfd898b6740d855ef36ad55bee6c88fb71d
= <michaelhagarty@gmail.com>
2018-07-09 20:09:58 +1000

    Add balance to constructor. Allow deep copy.

--- account/account.py
+++ account/account.py
@@ -1,4 +1,7 @@
 
+from __future__ import annotations
+
+from copy import deepcopy
 from typing import List
 from uuid import UUID
 
@@ -11,16 +14,37 @@ class Account(Entity):
     class InsufficientBalance(Exception):
         pass
 
-    def __init__(self, id: UUID = None, owner: UUID = None) -> None:
+    def __init__(self, id: UUID = None, owner: UUID = None,
+                 balance: AUD = None) -> None:
         super(Account, self).__init__(id=id)
 
         if owner is None:
             raise ValueError("An account must have an owner.")
 
         self.transactions: List[Transaction] = []
-        self.balance = AUD('0')
+        self.balance = AUD('0') if balance is None else balance
         self.owner = owner
         self.can_overdraw = False
+
+    def __copy__(self) -> Account:
+        new_copy = type(self)(id=self.id, owner=self.owner)
+        new_copy.balance = self.balance
+        new_copy.transactions = self.transactions
+
+        return new_copy
+
+    def __deepcopy__(self, memo) -> Account:
+        self_id = id(self)
+        if self_id in memo:
+            return memo[self_id]
+
+        new_id = deepcopy(self.id)
+        new_owner = deepcopy(self.owner)
+        new_account = type(self)(id=new_id, owner=new_owner)
+        new_account.balance = deepcopy(self.balance)
+        new_account.transactions = deepcopy(self.transactions)
+
+        return new_account
 
     def _add_transaction(self, transaction: Transaction) -> None:
         self.transactions.append(transaction)
@@ -50,7 +74,8 @@ class RegularAccount(Account):
 
 
 class ExternalCounterparty(Account):
-    def __init__(self, id: UUID = None, owner: UUID = None) -> None:
-        super(ExternalCounterparty, self).__init__(id, owner)
+    def __init__(self, id: UUID = None, owner: UUID = None,
+                 balance: AUD = None) -> None:
+        super(ExternalCounterparty, self).__init__(id, owner, balance)
 
         self.can_overdraw = True
