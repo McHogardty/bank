7d09a9bf33b88572adf50ad8e280582bcd10b225
Michael Hagarty <michaelhagarty@gmail.com>
2018-09-18 10:00:03 +1000

    Fix up type annotations and support work outside of transaction.

--- infrastructure/memory_store.py
+++ infrastructure/memory_store.py
@@ -1,10 +1,11 @@
 
 import copy
-from typing import Any, Callable, Dict, Iterable
+from typing import Any, Callable, Dict, Iterator, Optional
 from uuid import UUID
 
 StorageRecord = Dict[str, Any]
 RecordStore = Dict[UUID, StorageRecord]
+Store = Dict[str, RecordStore]
 SearchPredicate = Callable[[StorageRecord], bool]
 
 
@@ -14,15 +15,15 @@ class NotFound(Exception):
 
 class MemoryStore:
     def __init__(self):
-        self._records: Dict[str, RecordStore] = {}
-        self._changed: Dict[str, RecordStore] = {}
-        self._deleted: Dict[str, RecordStore] = {}
+        self._records: Store = {}
+        self._changed: Optional[Store] = None
 
     def get(self, record_type: str, key: UUID) -> StorageRecord:
-        try:
-            return copy.deepcopy(self._changed[record_type][key])
-        except KeyError:
-            pass
+        if self._changed:
+            try:
+                return copy.deepcopy(self._changed[record_type][key])
+            except KeyError:
+                pass
 
         try:
             return copy.deepcopy(self._records[record_type][key])
@@ -31,12 +32,14 @@ class MemoryStore:
 
     def find(self,
              record_type: str,
-             predicate: SearchPredicate) -> Iterable[StorageRecord]:
-        changed_records = self._changed.get(record_type, {})
+             predicate: SearchPredicate) -> Iterator[StorageRecord]:
+        changed_records: RecordStore = {}
+        if self._changed is not None:
+            changed_records = self._changed.get(record_type, {})
         existing_records = self._records.get(record_type, {})
 
         if not changed_records and not existing_records:
-            return []
+            return iter([])
 
         return (copy.deepcopy(r)
                 for r in {**existing_records, **changed_records}.values()
@@ -46,18 +49,31 @@ class MemoryStore:
             record_type: str,
             key: UUID,
             record: StorageRecord) -> None:
-        self._changed.setdefault(record_type, {})[key] = copy.deepcopy(record)
+        record = copy.deepcopy(record)
+        if self._changed is not None:
+            self._changed.setdefault(record_type, {})[key] = record
+        else:
+            self._records.setdefault(record_type, {})[key] = record
 
     def update(self,
                record_type: str,
                key: UUID,
                record: StorageRecord) -> None:
-        self._changed.setdefault(record_type, {})[key] = copy.deepcopy(record)
+        record = copy.deepcopy(record)
+        if self._changed is not None:
+            self._changed.setdefault(record_type, {})[key] = record
+        else:
+            self._records.setdefault(record_type, {})[key] = record
+
+    def begin(self) -> None:
+        self._changed = {}
 
     def rollback(self) -> None:
-        self._changed = {}
-        self._deleted = {}
+        self._changed = None
 
     def commit(self) -> None:
-        for k, v in self._changed.items():
-            self._records[k].update(v)
+        if self._changed is not None:
+            for k, v in self._changed.items():
+                self._records.setdefault(k, {}).update(v)
+
+        self._changed = None
