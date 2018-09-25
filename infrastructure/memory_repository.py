8e00dbba121fb38da78e83cf09f8cd0bb67921c3
Michael Hagarty <michaelhagarty@gmail.com>
2018-09-25 18:13:19 +1000

    Modify repository to handle new account model and define new method.

--- infrastructure/memory_repository.py
+++ infrastructure/memory_repository.py
@@ -1,14 +1,17 @@
 
 from enum import auto, Enum
-from typing import Dict
+from typing import Dict, List, Type
 from uuid import UUID
 
 from account import (
     Account,
     Card,
-    CardAccount,
+    CardNumber,
+    CardSubAccount,
     ExternalCounterparty,
     RegularAccount,
+    RegularSubAccount,
+    SubAccount,
     Transaction,
 )
 from account.repository import AccountRepository
@@ -25,14 +28,25 @@ class AccountType(Enum):
     CARD = auto()
 
 
-account_types = {
+class SubAccountType(Enum):
+    REGULAR = auto()
+    CARD = auto()
+
+
+account_types: Dict[Type[Account], AccountType] = {
     ExternalCounterparty: AccountType.EXTERNAL_COUNTERPARTY,
     RegularAccount: AccountType.REGULAR,
-    CardAccount: AccountType.CARD
+}
+
+
+subaccount_types: Dict[Type[SubAccount], SubAccountType] = {
+    CardSubAccount: SubAccountType.CARD,
+    RegularSubAccount: SubAccountType.REGULAR,
 }
 
 
 ACCOUNT_MODEL = 'account'
+SUBACCOUNT_MODEL = 'subaccount'
 TRANSACTION_MODEL = 'transaction'
 CARD_MODEL = 'card'
 
@@ -46,14 +60,21 @@ class InMemoryRepository(AccountRepository):
         return {
             'id': account.id,
             'owner': account.owner,
-            'balance': account.balance,
             'type': account_types[type(account)],
-            'parent': None if account.parent is None else account.parent.id,
+        }
+
+    def _subaccount_to_record(self,
+                              subaccount: SubAccount,
+                              account: Account) -> StorageRecord:
+        return {
+            'id': subaccount.id,
+            'account': account.id,
+            'type': subaccount_types[type(subaccount)]
         }
 
     def _transaction_to_record(self,
                                transaction: Transaction,
-                               account: Account) -> StorageRecord:
+                               account: SubAccount) -> StorageRecord:
         return {
             'id': transaction.id,
             'reference': transaction.reference,
@@ -62,7 +83,9 @@ class InMemoryRepository(AccountRepository):
             'account': account.id,
         }
 
-    def _card_to_record(self, card: Card, account: Account) -> StorageRecord:
+    def _card_to_record(self,
+                        card: Card,
+                        account: SubAccount) -> StorageRecord:
         return {
             'id': card.id,
             'number': card.number,
@@ -90,38 +113,56 @@ class InMemoryRepository(AccountRepository):
             raise ValueError('Encountered account record with unknown account'
                              ' type {}'.format(record_type))
 
-        def find_transaction(record):
-            return record['account'] == account_id
+        subaccounts: List[SubAccount] = []
+        subaccount_records = list(self._store.find(
+            SUBACCOUNT_MODEL,
+            lambda r: r['account'] == account_id,
+        ))
+        # print(subaccount_records)
+        for subaccount_record in subaccount_records:
+            # print("Got subaccount.")
+            subaccount_class = None
+            record_type = subaccount_record.pop('type')
+            subaccount_record.pop('account')
+            subaccount_id = subaccount_record['id']
 
-        transaction_records = self._store.find(
-            TRANSACTION_MODEL,
-            lambda r: r["account"] == account_id,
-        )
+            for class_, subaccount_type in subaccount_types.items():
+                if record_type == subaccount_type:
+                    subaccount_class = class_
 
-        transactions = []
-        for t in transaction_records:
-            t.pop('account')
-            transactions.append(Transaction(**t))
+            if subaccount_class is None:
+                raise ValueError('Encountered unknown account type for '
+                                 'account {}. Type was {}.'
+                                 .format(account_id, record_type))
 
-        record['transactions'] = transactions
+            transaction_records = self._store.find(
+                TRANSACTION_MODEL,
+                lambda r: r['account'] == subaccount_id,
+            )
 
-        if account_class == CardAccount:
-            result = list(self._store.find(
-                CARD_MODEL,
-                lambda c: c['account'] == account_id
-            ))
-            if len(result) != 1:
-                raise ValueError('Encountered a card account {} with no '
-                                 'associated card.'.format(account_id))
-            card_record = result[0]
-            card_record.pop('account')
-            record['card'] = Card(**card_record)
+            transactions: List[Transaction] = []
+            for t in transaction_records:
+                t.pop('account')
+                transactions.append(Transaction(**t))
 
-        parent = record.pop('parent')
-        if parent is not None:
-            parent = self.get(parent)
-            record['parent'] = parent
+            subaccount_record['transactions'] = transactions
 
+            if subaccount_class == CardSubAccount:
+                card_records = list(self._store.find(
+                    CARD_MODEL,
+                    lambda c: c['account'] == subaccount_id,
+                ))
+                if len(card_records) != 1:
+                    raise ValueError('Encountered a card sub account {}'
+                                     'with no associated card.'
+                                     .format(subaccount_id))
+                card_record = card_records[0]
+                card_record.pop('account')
+                subaccount_record['card'] = Card(**card_record)
+
+            subaccounts.append(subaccount_class(**subaccount_record))
+
+        record['subaccounts'] = subaccounts
         self._cache[account_id] = instance = account_class(**record)
         return instance
 
@@ -129,16 +170,17 @@ class InMemoryRepository(AccountRepository):
         record = self._account_to_record(account)
         self._store.add(ACCOUNT_MODEL, account.id, record)
 
-        for t in account.transactions:
-            record = self._transaction_to_record(t, account)
-            self._store.add(TRANSACTION_MODEL, t.id, record)
+        for s in account.subaccounts:
+            record = self._subaccount_to_record(s, account)
+            self._store.add(SUBACCOUNT_MODEL, s.id, record)
 
-        if isinstance(account, CardAccount):
-            record = self._card_to_record(account.card, account)
-            self._store.add(CARD_MODEL, account.card.id, record)
+            if isinstance(s, CardSubAccount):
+                record = self._card_to_record(s.card, s)
+                self._store.add(CARD_MODEL, s.card.id, record)
 
-        if account.parent is not None:
-            self.add(account.parent)
+            for t in s.transactions:
+                record = self._transaction_to_record(t, s)
+                self._store.add(TRANSACTION_MODEL, t.id, record)
 
         self._cache[account.id] = account
 
@@ -146,11 +188,37 @@ class InMemoryRepository(AccountRepository):
         record = self._account_to_record(account)
         self._store.update(ACCOUNT_MODEL, account.id, record)
 
-        for t in account.transactions:
-            record = self._transaction_to_record(t, account)
-            self._store.update(TRANSACTION_MODEL, t.id, record)
+        for s in account.subaccounts:
+            record = self._subaccount_to_record(s, account)
+            self._store.update(SUBACCOUNT_MODEL, s.id, record)
 
-        if account.parent is not None:
-            self.update(account.parent)
+            if isinstance(s, CardSubAccount):
+                record = self._card_to_record(s.card, s)
+                self._store.update(CARD_MODEL, s.card.id, record)
+
+            for t in s.transactions:
+                record = self._transaction_to_record(t, s)
+                self._store.update(TRANSACTION_MODEL, t.id, record)
 
         self._cache[account.id] = account
+
+    def find_by_card_number(self, card_number: CardNumber) -> Account:
+        card_records = list(self._store.find(
+            CARD_MODEL,
+            lambda c: c['number'] == card_number,
+        ))
+
+        if len(card_records) != 1:
+            raise ValueError('Could not find a card record for card number {}'
+                             .format(card_number))
+
+        subaccount_id = card_records[0]['account']
+
+        try:
+            subaccount = self._store.get(SUBACCOUNT_MODEL, subaccount_id)
+        except Exception:
+            raise AccountRepository.DoesNotExist('Could not find a subaccount '
+                                                 'with ID {}.'
+                                                 .format(subaccount_id))
+
+        return self.get(subaccount['account'])
